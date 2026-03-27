aea2b858cc31b0de55cd6435a3db3551ca36901d
julia11862139-alt <julia.11862139@aluno.mg.gov.br>
2026-03-27 07:43:39 -0300

    Add files via upload

---
+++ helloword.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+public static void main(String[] args) {
+System.out.println("Olá, Mundo! Meu nome é Julia Moreira e este é meu primeiro programa no GitHub.");
+System.out.println("Turma:2°1");
+}
+}
